681df0dda73ff7adb9c5ca57d9014691edd839d2
axplpxa <developer.zkrk@gmail.com>
2024-06-23 22:57:50 +0300

    First Practic Task

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log(2);
-console.log(4);
+const numberOffilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+
+let personalMovieDB = {
+    count : numberOffilms ,
+    movies : { },
+    actors : { },
+    genres : [ ],
+    privat : false 
+}
+
+let ans1_1 = prompt("Один из последних просмотренных фильмов ?",'');
+let ans1_2 = prompt("Насколько его оцените?",'');
+
+let ans2_1 = prompt("Один из последних просмотренных фильмов ?",'');
+let ans2_2 = prompt("Насколько его оцените?",'');
+
+personalMovieDB.movies[ans1_1] = ans1_2;
+personalMovieDB.movies[ans2_1] = ans2_2;
+
+console.log(personalMovieDB);
